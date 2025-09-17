2219a1266a4cf115f3f6f16ec566053057ad7702
Luis Sousa <las.sousa@outlook.com>
2025-09-17 16:27:22 -0300

    feat: create order entity

--- src/main/java/com/luis/dscommerce/entities/User.java
+++ src/main/java/com/luis/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.luis.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -15,6 +17,9 @@ public class User {
     private String phone;
     private LocalDate birthdate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     private User(){}
 
@@ -73,5 +78,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
